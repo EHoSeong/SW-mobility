84ee494451040fdbb6db8e9df9775bde77d3c4ce
HS <lhs06185@gmail.com>
2025-05-16 14:21:37 +0900

    0516

---
+++ level2/큰 수 만들기.py
@@ -0,0 +1,13 @@
+# def solution(number, k):
+#     answer = ''
+#     num_arr = sorted([i for i in number], reverse=True)
+#     while k>0:
+
+
+
+#     return answer
+
+# number = '19241'
+# k = 2
+# print(solution(number,k))
+
